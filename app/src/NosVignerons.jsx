aa8d792389e010c0b011232ce47f61ad556cb1d4
juleau14 <jcamuzet@yahoo.com>
2023-05-02 16:48:03 +0200

    fixing some nav pb and adding nos vignerons page

--- app/src/NosVignerons.jsx
+++ app/src/NosVignerons.jsx
@@ -1,14 +1,111 @@
 import NavMenu from "./components/NavMenu";
-import React from "react";
+import React, { useState } from "react";
+import "./NosVignerons.css";
+import vigneronsData from "./vigneronsData.json";
+
+const data = Object.entries(vigneronsData);
+
+const NosVigneronsBanner = () => {
+  return (
+    <React.Fragment>
+      <div className="banner">
+        <div className="nv-background-container"></div>
+        <h1 className="nv-title">
+          Nos
+          <br />
+          Vignerons
+        </h1>
+        <button className="nv-scroll-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="lightyellow"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
+            />
+          </svg>
+        </button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const NosVigneronsList = () => {
+  return (
+    <section className="nv-list-section">
+      <div className="nv-list">
+        {data.map((regionData, index) => {
+          const regionName = regionData[0];
+          const vigneronsArr = regionData[1];
+          return (
+            <NosVigneronsListItem
+              key={index}
+              regionName={regionName}
+              vigneronsArr={vigneronsArr}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+const NosVigneronsListItem = (props) => {
+  const regionName = props.regionName;
+  const vigneronsArr = props.vigneronsArr;
+  const nbVignerons = props.vigneronsArr.length;
+
+  const [opened, setOpened] = useState(false);
+
+  const handleClick = () => {
+    opened ? setOpened(false) : setOpened(true);
+  };
+
+  return (
+    <div className="nv-list-item">
+      <h2 className="nv-list-item-title" onClick={handleClick}>
+        <span>{regionName}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={opened ? "visible" : ""}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </h2>
+      <ul
+        className="nv-list-item-content"
+        style={opened ? { height: `${50 * nbVignerons}px` } : { height: "0px" }}
+      >
+        {vigneronsArr.map((vigneronName, index) => {
+          return <li key={vigneronName}>{vigneronName}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 const NosVignerons = () => {
   return (
     <React.Fragment>
-      <NavMenu />
-      <div></div>
+      <NavMenu navButtonColor={"lightyellow"} />
+      <NosVigneronsBanner />
+      <NosVigneronsList />
     </React.Fragment>
-  )
-}
-
+  );
+};
 
 export default NosVignerons;
